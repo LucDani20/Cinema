cf5f0f3c62cb93be561a77b61e2e4daf6b6bda1b
unknown <lucdanielrh01@gmail.com>
2026-08-02 18:58:08 +0300

    feat: add Genre and UserRole enums

---
+++ src/main/java/td/cine/demo/repository/model/Genre.java
@@ -0,0 +1,12 @@
+package td.cine.demo.repository.model;
+
+public enum Genre {
+    THRILLER,
+    ROMANCE,
+    COMEDY,
+    DRAMA,
+    ACTION,
+    SCI_FI,
+    FANTASY,
+    ANIMATION
+}
---
+++ src/main/java/td/cine/demo/repository/model/UserRole.java
@@ -0,0 +1,7 @@
+package td.cine.demo.repository.model;
+
+public enum UserRole {
+    CLIENT,
+    EMPLOYEE,
+    MANAGER
+}
